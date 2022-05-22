46ed423a7b40261dd7720d25f8b27c287e54a5e1
karan <karan.mishra_cs19@gla.ac.in>
2022-05-23 00:13:32 +0530

    initial commit

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+import Input from "./components/Input/Input";
+import MovieResults from "./components/MovieResults/MovieResults";
+import Popup from "./components/Popup/Popup";
+
+const App = () => {
+  // Form input state
+  const [inputValue, setInputValue] = useState("");
+  const [formResults, setFormResults] = useState({ results: [], selected: {} });
+
+  const api = "http://www.omdbapi.com/?apikey=192ca557";
+
+  // Get the input value from user and update state
+  const handleInputValue = (e) => {
+    const userInput = e.target.value;
+    setInputValue(userInput);
+  };
+
+  // Connect to api and get data
+  const handleFormResults = async (e) => {
+    e.preventDefault();
+    if (inputValue.trim() !== "") {
+      await axios(api + "&s=" + inputValue).then(({data}) => {
+        let results = data.Search
+        // check if user enters an empty string
+        setFormResults((prevState) => {
+          return { ...prevState, results: results };
+        });
+      });
+    }
+    setInputValue("")
+  };
+
+  // Open popup
+  const openPopup = (id) => {
+    axios(api + "&i=" + id).then(({ data }) => {
+      let result = data;
+      console.log(data);
+      setFormResults((prevState) => {
+        return { ...prevState, selected: result };
+      });
+    });
+  };
+
+  // Close popup
+  const closePopup = () => {
+    setFormResults((prevState) => {
+      return { ...prevState, selected: {} };
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" css={styles}>
+      <h1>React Movie Library</h1>
+      <Input
+        value={inputValue}
+        handleInputValue={handleInputValue}
+        handleFormResults={handleFormResults}
+      />
+      <MovieResults results={formResults} openPopup={openPopup} />
+      {typeof formResults.selected.Title !== "undefined" ? (
+        <Popup selected={formResults.selected} closePopup={closePopup} />
+      ) : (
+        false
+      )}
     </div>
   );
-}
+};
+
+const styles = css`
+  background: blueviolet;
+  min-height: 100vh;
+  h1 {
+    text-align: center;
+    font-size: 40px;
+    padding: 10px 0;
+  }
+`;
 
 export default App;
